1e2fa06dc3ff16c3c8187cd4dbfd38643d35c4b7
Pjai1 <ulti40@hotmail.com>
2017-09-15 19:16:27 +0200

    Added Post, Put, Patch, Delete methods

--- CitiesDataStore.cs
+++ CitiesDataStore.cs
@@ -21,72 +21,66 @@ namespace CoreApi
 
                 new CityDto()
                 {
-                    Id=1,
-                    Name="New York City",
-                    Description="The one that big park",
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one that big park",
                     PointsOfInterest = new List<PointOfInterestDto>()
                     {
                         new PointOfInterestDto()
                         {
                             Id = 1,
-                            Name="Central Park",
-                            Description = "The most visited urban park in the United States." },
-
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States." 
+                        },
                         new PointOfInterestDto()
                         {
                             Id = 2,
-                            Name="Empire State Building",
-                            Description = "A 102-story skyscraper located in Midtown Manhattan." },
-
-
-
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan." 
+                        },
                     }
                 },
 
                 new CityDto()
                 {
                     Id=2,
-                    Name="Antwrep",
-                    Description="The one that big park",PointsOfInterest = new List<PointOfInterestDto>()
+                    Name = "Antwrep",
+                    Description = "The one that big park",PointsOfInterest = new List<PointOfInterestDto>()
                     {
                         new PointOfInterestDto()
                         {
-                            Id = 1,
-                            Name="Central Park",
-                            Description = "The most visited urban park in the United States." },
-
+                            Id = 3,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States." 
+                        },
                         new PointOfInterestDto()
                         {
-                            Id = 2,
-                            Name="Empire State Building",
-                            Description = "A 102-story skyscraper located in Midtown Manhattan." },
-
-
-
+                            Id = 4,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan." 
+                        },
                     }
                 },
 
                 new CityDto()
                 {
-                    Id=3,
-                    Name="Paris",
-                    Description="The one that big park",
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one that big park",
                     PointsOfInterest = new List<PointOfInterestDto>()
                     {
                         new PointOfInterestDto()
                         {
-                            Id = 1,
-                            Name="Central Park",
-                            Description = "The most visited urban park in the United States." },
-
+                            Id = 5,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States." 
+                        },
                         new PointOfInterestDto()
                         {
-                            Id = 2,
-                            Name="Empire State Building",
-                            Description = "A 102-story skyscraper located in Midtown Manhattan." },
-
-
-
+                            Id = 6,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan." 
+                        },
                     }
                 }
             };
--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.JsonPatch;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using CoreApi.Models;
 
 namespace CoreApi.Controllers
 {
@@ -22,7 +25,7 @@ namespace CoreApi.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        [HttpGet("{cityId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -40,6 +43,158 @@ namespace CoreApi.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if(pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if(pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description cannot be the same as the name");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, id = finalPointOfInterest.Id }, finalPointOfInterest);
+        }
+
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if(pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if(pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description cannot be the same as the name");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if(pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{cityId}/pointsofinterest/{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id, [FromBody] JsonPatchDocument<PointOfInterestForUpdatingDto> patchDocument)
+        {
+            if(patchDocument == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if(pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestToPatch = new PointOfInterestForUpdatingDto()
+            {
+                Name = pointOfInterestFromStore.Name,
+                Description = pointOfInterestFromStore.Description
+            };
+
+            patchDocument.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(pointOfInterestToPatch.Description == pointOfInterestToPatch.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description cannot be the same as the name");
+            }
+
+            TryValidateModel(pointOfInterestToPatch);
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}/pointsofinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if(pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
         }
     }
 }
